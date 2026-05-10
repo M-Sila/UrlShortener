fc1408d4eae4643242c1bf8ebc76183fdd3deae7
M-Sila <majd.sila@outlook.de>
2026-05-10 23:08:04 +0200

    Add initial NUnit test project for UrlShortener

---
+++ UrlShortenerTest/LinkServiceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace UrlShortenerTest
+{
+    public class LinkServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
