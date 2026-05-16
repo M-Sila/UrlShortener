f8cbf85dc879838a9008122840352b48e794e309
M-Sila <majd.sila@outlook.de>
2026-05-16 23:57:53 +0200

    make the clean architecture in seperate projects

--- UrlShortener.Application/Services/ClickBackgroundService.cs
+++ UrlShortener.Application/Services/ClickBackgroundService.cs
@@ -1,4 +1,6 @@
-﻿using UrlShortener.Domain.Entities;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UrlShortener.Domain.Entities;
 using UrlShortener.Domain.Repositories;
 
 namespace UrlShortener.Application.Services
